92af8ceeaba1e74c3cd97b9e53b836605f196224
Bokor Armin <abokor@terc.hu>
2025-04-25 18:55:49 +0200

    Add react router config

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
 import {initI18n} from "./i18n/i18n.ts";
 import {RouterProvider} from 'react-router-dom';
+import {router} from "./app/router.tsx";
 
 initI18n();
 
